57aa734c855abd07ef97f5a8bbceafd44bb9b96b
groupwhiteroomit <groupwhiteroom@gmail.com>
2024-08-19 13:35:45 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,10 +9,10 @@ DB_LOCATION = os.path.join(os.getcwd(), "TA.db").replace("\\", "/")
 
 # Super HR/Admin details
 SUPER_HR = {
-    "employee_id": os.environ.get("HR12345678"),
-    "fullname": os.environ.get("HR"),
+    "employee_id": os.environ.get("SUPER_HR_EMP_ID"),
+    "fullname": os.environ.get("SUPER_HR_NAME"),
     "role": "HR",
-    "temp_pwd": os.environ.get("13245678"),
+    "temp_pwd": os.environ.get("SUPER_HR_PWD"),
     "last_chat_id": "",
     "is_active": True,
     "is_pwd_expired": False,
